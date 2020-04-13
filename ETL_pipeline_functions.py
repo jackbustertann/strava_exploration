58b5e51e2b80d5c956450d5c4de0e6380a16ccc0
Jack Tann <jackbustertann@icloud.com>
2020-04-13 15:27:40 +0100

    changing local directory

--- ETL_pipeline_functions.py
+++ ETL_pipeline_functions.py
@@ -3,26 +3,33 @@ import pandas as pd
 import requests, json, csv
 from datetime import datetime, timedelta
 import time
+import re
 
-def last_date(activities_file):
+def last_timestamp(activities_file):
 
     with open(activities_file, 'r') as f:
         lines = f.read().splitlines()
         first_line = lines[0].split(',')
         last_line = lines[-1].split(',')
         last_line_dict = dict(list(zip(first_line, last_line)))
-        last_date = last_line_dict['start_date']
+        last_timestamp = last_line_dict['timestamp']
         f.close()
 
-    return last_date
+    return last_timestamp
 
-def iso_8601_to_unix(date_string):
+def sql_timestamp_formatter(timestamp_iso_string):
 
-    iso_8601_date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
-    date_tuple = iso_8601_date.timetuple()
-    unix_date = int(time.mktime(date_tuple))
+    timestamp_datetime = datetime.strptime(timestamp_iso_string, "%Y-%m-%dT%H:%M:%SZ")
+    timestamp_sql = timestamp_datetime.strftime('%Y-%m-%d %H:%M:%S')
+    return timestamp_sql
 
-    return unix_date
+def timestamp_to_unix(timestamp_string):
+
+    timestamp_datetime = datetime.strptime(timestamp_string, "%Y-%m-%d %H:%M:%S")
+    datetime_tuple = timestamp_datetime.timetuple()
+    unix_timestamp = int(time.mktime(datetime_tuple))
+
+    return unix_timestamp
 
 def strava_token_exchange(credentials_file):
 
@@ -67,22 +74,24 @@ def request_location(geocode_key, latlng):
 
     return location
 
-def request_activities(strava_access_token, geocode_key, start_date):
+def request_activities(strava_access_token, geocode_key, start_date = False):
 
     url = "https://www.strava.com/api/v3/athlete/activities"
     headers = {"Authorization": "Bearer {}".format(strava_access_token)}
-    params = {'after': start_date}
+    params = {}
+    if start_date:
+        params['after'] = start_date
     req = requests.get(url, headers = headers, params = params).json()
 
     activities = []
 
     for activity in req:
         activity_info = {}
-        activity_info['activity_id'] = activity['id']
-        activity_info['start_date'] = activity['start_date_local']
+        activity_info['id'] = activity['id']
+        activity_info['timestamp'] = sql_timestamp_formatter(activity['start_date_local'])
         activity_info['activity_name'] = activity['name']
         activity_info['activity_type'] = activity['type']
-        activity_info['distance'] = activity['distance']
+        activity_info['distance'] = activity['distance'] / 1000
         activity_info['time'] = activity['elapsed_time']
 
         latlng = activity.get('start_latlng')
@@ -92,13 +101,18 @@ def request_activities(strava_access_token, geocode_key, start_date):
             activity_info['location'] = 'missing'
 
         activity_info['elevation_gain'] = activity.get('total_elevation_gain', 0)
-        activity_info['average_speed'] = activity.get('average_speed', 0)
-        activity_info['max_speed'] = activity.get('max_speed', 0)
+        activity_info['average_speed'] = activity.get('average_speed', 0) * 3.6
+        activity_info['max_speed'] = activity.get('max_speed', 0) * 3.6
         activity_info['average_hr'] = activity.get('average_heartrate', 0)
         activity_info['max_hr'] = activity.get('max_heartrate', 0)
         activity_info['average_cadence'] = activity.get('average_cadence', 0)
         activity_info['kudos'] = activity.get('kudos_count', 0)
-        activity_info['suffer_score'] = activity.get('suffer_score', 0)
+
+        if activity.get('suffer_score', None):
+            activity_info['suffer_score'] = activity['suffer_score']
+        else:
+            activity_info['suffer_score'] = 0
+
         activities.append(activity_info)
 
     return activities
@@ -116,11 +130,11 @@ def request_splits(strava_access_token, activity_ids):
             split_info = {}
             split_info['activity_id'] = activity_id
             split_info['split_index'] = split['split']
-            split_info['split_distance'] = split['distance']
+            split_info['split_distance'] = split['distance'] / 1000
             split_info['split_time'] = split['elapsed_time']
             split_info['split_elevation_gain'] = split.get('total_elevation_gain', 0)
-            split_info['split_average_speed'] = split.get('average_speed', 0)
-            split_info['split_max_speed'] = split.get('max_speed', 0)
+            split_info['split_average_speed'] = split.get('average_speed', 0) * 3.6
+            split_info['split_max_speed'] = split.get('max_speed', 0) * 3.6
             split_info['split_average_hr'] = split.get('average_heartrate', 0)
             split_info['split_max_hr'] = split.get('max_heartrate', 0)
             split_info['split_average_cadence'] = split.get('average_cadence', 0)
@@ -166,3 +180,69 @@ def append_requests(requests, file_name):
         a.close()
 
     return print("{} appended".format(file_name))
+
+def get_run_type(activity):
+    key_words_int = ['Intervals', 'Yasso', 'Track']
+    key_words_wu = ['WU', 'test']
+    for key_word in key_words_int:
+        if key_word in activity['activity_name']:
+            return 'I'
+    for key_word in key_words_wu:
+        if key_word in activity['activity_name']:
+            return 'WU'
+    if 'WD' in activity['activity_name']:
+            return 'WD'
+    elif activity['distance'] < 8:
+        return 'S'
+    elif activity['distance'] < 16:
+        return 'M'
+    else:
+        return 'L'  
+
+def get_position(activity):
+    pos_pattern = re.compile('.*\(\d?:?\d{2}:\d{2}\s-\s(\d+)\w+\)')
+    pos_pattern_match = re.findall(pos_pattern, activity['activity_name'])
+    if pos_pattern_match:
+        return int(pos_pattern_match[0])
+    else:
+        return 0
+
+def get_event_type(activity):
+    if ('PR' in activity['activity_name']) & (activity['run_type'] == 'S') :
+        return 'PR'
+    elif activity['position'] > 0:
+        return 'R'
+    else:
+        return 'W'
+
+def HHMMSS_to_seconds(time_HHMMSS):
+    time_tuple = [0] * 3
+    time_tuple_raw = [int(x) for x in time_HHMMSS.split(':')]
+    time_tuple[-len(time_tuple_raw):] = time_tuple_raw
+    time_seconds = (time_tuple[0] * 3600) + (time_tuple[1] * 60) + time_tuple[2]
+    return time_seconds
+
+def get_chip_time(activity):
+    time_pattern = re.compile('.*\((\d?:?\d{2}:\d{2})\s-\s\d+\w+\)')
+    time_pattern_match = re.findall(time_pattern, activity['activity_name'])
+    if time_pattern_match:
+        return HHMMSS_to_seconds(time_pattern_match[0])
+    else:
+        return activity['time']
+
+def activity_cleaner(activity):
+    cleaned_activity = activity.copy()
+
+    ## binning activities based on distances
+    cleaned_activity['run_type'] = get_run_type(cleaned_activity)
+    ## extracting race positions from activity names
+    cleaned_activity['position'] = get_position(cleaned_activity)
+    ## extracting event types from activity names
+    cleaned_activity['event_type'] = get_event_type(cleaned_activity)
+    ## extracting race chip times from activity names
+    cleaned_activity['chip_time'] = get_chip_time(cleaned_activity)
+
+    # dropping activity names
+    cleaned_activity.pop('activity_name', None)
+
+    return cleaned_activity
